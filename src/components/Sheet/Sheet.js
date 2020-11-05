97d948c9015545353031291407a9ef5dc33dd77d
Huit Carlos <emox2544@gmail.com>
2020-11-04 21:51:08 -0600

    questions

--- src/components/Sheet/Sheet.js
+++ src/components/Sheet/Sheet.js
@@ -2,6 +2,8 @@ import React from 'react';
 import IconReliancera from '../../assets/reliancera-icon.png';
 import Card from '../Card/Card';
 import { data } from '../../data';
+import styles from './Sheet.module.css';
+import { Timeline, Input, Toggle, InputPicker, TagPicker } from 'rsuite'
 
 const Annotation = ({ text }) => {
 
@@ -19,33 +21,112 @@ const Block = ({ children }) => {
   )
 }
 
+const QuestionStr = ({ text }) => (
+  <div style={{ display: 'grid', gridTemplateColumns: '1fr 300px' }}>
+    <div>{text}</div>
+    <div><Input/> </div>
+  </div>
+)
+
+const QuestionBool = ({ text }) => (
+  <div style={{ display: 'grid', gridTemplateColumns: '1fr 300px' }}>
+    <div>{text}</div>
+    <div><Toggle size="lg" checkedChildren="Yes" unCheckedChildren="No" /></div>
+  </div>
+)
+
+const QuestionTwoFields = ({ text }) => (
+  <div style={{ display: 'grid', gridTemplateColumns: '1fr 300px' }}>
+    <div>{text}</div>
+    <div style={{ display: 'grid', gridTemplateColumns: '120px 170px', columnGap: '10px', alignItems: 'center' }}>
+      <div style={{ display: 'grid', alignItems: 'center', gridTemplateColumns: '60px 1fr' }}> Children: <Input/> </div>
+      <div style={{ display: 'grid', alignItems: 'center', gridTemplateColumns: '100px 1fr' }}> Grandchildren: <Input/> </div>
+    </div>
+  </div>
+)
+
+const QuestionPicker = ({ text, opts }) => (
+  <div style={{ display: 'grid', gridTemplateColumns: '1fr 300px' }}>
+    <div>{text}</div>
+    <InputPicker size="md"
+      placeholder="Select..."
+      data={opts}/>
+  </div>
+)
+
 export default function Sheet ({}) {
 
-  const { outbound, inbound }  = data;
+  const { two, inbound, peg }  = data;
 
   return (
-    <div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div className={styles.Container}>
+
+      <div className={styles.Header}>
         <img width="300" src={IconReliancera} alt="icon"/>
+        <h3 className={styles.ScriptTitle}>Final Expense Script</h3>
       </div>
 
-      <p style={{ textAlign: 'center', margin: '18px 0px !important' }}>I AM A TELESALES BEAST </p>
+      <p className={styles.SP}>I AM A TELESALES BEAST </p>
 
       <div style={{ display: 'grid', gridAutoRows: 'max-content', rowGap: '18px' }}>
 
-        <Card title={outbound.title}>{ outbound.content.map(text => (<p>{ text } </p> )) }</Card>
 
-        <Card title={outbound.title}>{ inbound.content.map(text => (<p>{ text } </p> )) }</Card>
+        <Card title={inbound.title}>{ inbound.content.map(text => (<p>{ text } </p> )) }</Card>
 
-        <Block>
-          <Annotation text={'Call Control: Just want info/quote? “That’s exactly what I’m here for.”  Pivot back to script'}/>
-          <Annotation text={'How I’m Going to Serve Your Need'}/>
-        </Block>
+        {/* <Annotation text="How I’m Going to Serve Your Need"/> */}
 
-        <Card title={outbound.title}>
-          
+        <Card title={two.title}>
+          <div>
+            <p>{two.content[0]}</p>
+            <p>{two.content[1]}</p>
+            <br/>
+            <Timeline style={{ paddingLeft: '24px' }}>
+              <Timeline.Item>We help thousands of people just like you find the right policy.</Timeline.Item>
+              <Timeline.Item>We listen to your needs and find you a STATE APPROVED plan that is COMFORTABLE and AFFORDABLE for you each month.</Timeline.Item>
+              <Timeline.Item>We're simply going to save you time and money today over the phone.</Timeline.Item>
+            </Timeline>
+            <br/>
+          </div>
         </Card>
 
+        <Card title="PEG - Praise, Empathy, & Gratitude as You Build Rapport, Take Notes as You Listen">
+          <div>
+
+            <p>{peg.content[0]}</p>
+            <br/>
+
+            <div style={{ padding: '0 36px' }}>
+              <QuestionStr text="Speaking of fun, what do you do for fun? Favorite hobby?"/>
+              <QuestionStr text="How long have you lived in <Client’s State>?"/>
+              <QuestionStr text="Do you currently have Life Insurance?"/>
+            </div>
+            <br/>
+
+            <p>{peg.content[1]}</p>
+            <br/>
+            <p>{peg.content[2]}</p>
+            <br/>
+            <p>{peg.content[3]}</p>
+
+            <div style={{ padding: '0 36px' }}>
+              <br/>
+              <QuestionStr text="Who would be your beneficiary?"/>
+              <QuestionBool text="Are you married?"/>
+              <QuestionTwoFields text="How many children do you have? Grandchildren?"/>
+              <QuestionStr text="If I had to write you a check to pay off all your debt today plus pay for your final arrangements, how much do you think I might need to write that out for?"/>
+              <QuestionPicker 
+                text="Are you currently employed, retired, or on social security disability?"
+                opts={[
+                  { label: 'Employed', value: 'Employed' },
+                  { label: 'Retired', value: 'Retired' },
+                  { label: 'Social security disability', value: 'social security disability' },
+                ]}
+                />
+
+            </div>
+
+          </div>
+        </Card>
 
       </div>
 
--- src/data.js
+++ src/data.js
@@ -1,16 +1,25 @@
 export const data = {
-  outbound: {
-    title: 'The Opening for Fresh Leads in Outbound',
-    content: [
-      'Is this the famous <Client’s Name>? Hi <Client’s Name> this is _______ with Reliancera calling at your request for more information about our STATE APPROVED plans. I’m the licensed agent assigned to assist you with this today.',
-      'Great! Were you looking for this information for yourself or for someone else too?'
-    ]
-  },
   inbound: {
     title: 'The Opening for Inbound',
     content: [
-      'This is _______, in the Final Expense Department, who do I have the pleasure of serving today?',
+      'This is _______, in the Final Expense Department, how are you today?',
       'Great! Were you looking for this information for yourself or for someone else too?'
+    ]
+  },
+  two: {
+    title: "How I’m Going to Serve Your Need",
+    content: [
+      'Again, _<Client\'s name>_, I\'m going to help you find coverage today and we are on a recorded line.',
+      'I just want to make sure you have some background on us:'
+    ]
+  },
+  peg: {
+    title: "PEG - Praise, Empathy, & Gratitude as You Build Rapport, Take Notes as You Listen",
+    content: [
+      'My job is to advocate for you and I promise to make this fast and easy for you today, but most importantly, I promise to do my best to make this fun, fair enough?',
+      'Thank you for sharing that with me, now, <Clients Name> I\'m required by law to make sure you have my state license number. This way, you know I\'m licensed by the state to determine your eligibility today. My National license number is ______. (take your time, let me know when you\'re ready).',
+      'Thank you for writing that down. Now, I\'d love to know more about your situation to better understand how I can be of service. Let\'s walk through the things that your family might need to have relieved when that inevitable day comes.',
+      'First, let\'s discuss who you\'re wanting to protect, IF I can get you qualified TODAY:'
     ]
   },
 }
